d6b2fd50eca3c1f362d4a99adb5949870d9c9b58
metronomeow <90399391+metronomeow@users.noreply.github.com>
2022-08-20 00:51:42 +0300

    Wow, I can read files, fantastic!

--- Test_Elvis/main.cpp
+++ Test_Elvis/main.cpp
@@ -2,54 +2,67 @@
 #include<vector>
 #include<string>
 #include<filesystem>
+#include<fstream>
 
 namespace fs = std::filesystem;
 
-class FileNames {
+void ReadFiles(std::vector<std::u8string> in_filenames) {
+    std::ifstream F;
+    for (int i = 0; i < in_filenames.size(); i++) {
+        std::string text_from_file;
+        F.open(in_filenames[i]);
+        if (F.is_open()) {
+            F >> text_from_file;
+            std::cout << "Yes!!!" << std::endl;
+            std::cout << text_from_file;
+
+        }
+        F.close();
+    }
+}
+
+class Files {
 public:
-    FileNames();
-    FileNames(std::u8string name);
+    Files();
+    Files(std::u8string name);
     void Setter(std::u8string name);
 private:
     std::u8string name;
     std::string text;
 };
 
-FileNames::FileNames() {
+Files::Files() {
 
 }
 
-FileNames::FileNames(std::u8string name) {
+Files::Files(std::u8string name) {
     this->name = name;
 }
 
-void FileNames::Setter(std::u8string name) {
+void Files::Setter(std::u8string name) {
     this->name = name;
 }
 
 int main(int argc, char* argv[]) {
-    //std::string catalog_name;
-    if (argc == 3) {
 
-        unsigned int Counter=0;
-        std::vector<FileNames> Array_of_Files;
-        std::string Names;
-        std::vector<std::u8string> files_names;
-        std::string catalog_name = argv[1];
+    if (argc == 3) { 
+
+        std::vector<Files> array_of_files;//Vector of all text files in catalog (directory)
+        std::string output_filename = argv[2];//Name of output file
+        std::vector<std::u8string> input_filenames;//Vector of input filenames
+        std::string catalog_name = argv[1];//Name of catalog
+
         for (const auto& entry : fs::directory_iterator(catalog_name)) {
-            std::u8string path_string{entry.path().u8string()};
-            //std::string path;
-           // path_string << path;
-            //std::cout << path_string << std::endl;
-            files_names.push_back(path_string);
-            Counter++;
+            std::u8string path_string{entry.path().u8string()};            
+            input_filenames.push_back(path_string);
         }
 
+        ReadFiles(input_filenames);
         
-        
-            //FileNames file(files_names[i]);
-        for (int i = 0; i < Counter; i++) {
-            Array_of_Files[i].Setter(files_names[i]);
+        Files file;
+        for (int i = 0; i < input_filenames.size(); i++) {
+            file.Setter(input_filenames[i]);
+            array_of_files.push_back(file);
         }
         return EXIT_SUCCESS;
     }
